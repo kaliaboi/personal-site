d404716ee6a21b94552b6b023fcc250e0ed3511b
designdevhybrid <abhishekkalia.productdesign@gmail.com>
2023-07-21 12:38:45 -0400

    Add blog posts

--- components/post.tsx
+++ components/post.tsx
@@ -1,10 +1,19 @@
 "use client";
 import { FC } from "react";
 import { motion } from "framer-motion";
+import { PortableText, PortableTextComponents } from "@portabletext/react";
+import dayjs from "dayjs";
 
 interface postlistProps {
   post: any;
 }
+
+const portableComponents: PortableTextComponents = {
+  types: {},
+  block: {
+    p: ({ children }) => <p className="prose text-7xl">{children}</p>,
+  },
+};
 
 const Post: FC<postlistProps> = ({ post }) => {
   return (
@@ -18,7 +27,24 @@ const Post: FC<postlistProps> = ({ post }) => {
       }}
       className="mt-24 md:mt-48 container max-w-2xl text-zinc-900 dark:text-zinc-100 prose dark:prose-invert"
     >
-      <h1 className="text-[48px] font-bold">{post.name}</h1>
+      <h1 className="text-[48px] font-bold mb-3">{post.name}</h1>
+      <p className="mt-0">
+        Published on {dayjs(post.publishDate).format("MMMM D, YYYY")}
+      </p>
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.8,
+          delay: 0.7,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+        className="mt-12"
+      >
+        <div className="text-9xl">
+          <PortableText value={post.content} components={portableComponents} />
+        </div>
+      </motion.div>
       {/* <motion.div
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
